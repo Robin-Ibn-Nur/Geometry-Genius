c271708e920c93248ced27e53da86c6ac6f14b85
saifsammy <saifsammy653@gmail.com>
2023-02-19 20:52:10 +0600

    rectangle file code clean

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,5 +1,4 @@
 document.getElementById('second-button').addEventListener('click', function (e) {
-    // console.log(e.target.parentNode.parentNode.parentNode.children[2].children[1].children[0].value)
     const titleName = e.target.parentNode.parentNode.parentNode.children[0].innerText;
     const firstInput = e.target.parentNode.parentNode.parentNode.children[2].children[0].children[0].value;
     const secondInput = e.target.parentNode.parentNode.parentNode.children[2].children[1].children[0].value;
